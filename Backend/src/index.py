4eed6afa10ce054f4d7d1cce644a10d135b3457a
Ernest Gupik <ernestgupik@wp.pl>
2024-09-28 15:25:57 +0200

    Don't need that

--- Backend/src/index.py
+++ Backend/src/index.py
@@ -14,7 +14,3 @@ def index():
 def handle_message(msg):
     print(f"Message: {msg}")
     send(f"Server received: {msg}", broadcast=True)
-
-
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
