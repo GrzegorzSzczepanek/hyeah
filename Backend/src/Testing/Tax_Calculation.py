8669773be448ff1f98c58078e26331f3ec986b22
karolukasik765 <karolalukasik00@gmail.com>
2024-09-29 03:06:46 +0200

    Tax calculation

--- Backend/src/Testing/Tax_Calculation.py
+++ Backend/src/Testing/Tax_Calculation.py
@@ -1,3 +1,6 @@
+LOAN_TAX_RATE = 0.005
+CAR_PURCHASE_TAX_RATE = 0.02
+
 def calculate_pcc_tax(
     transaction_type,
     amount
@@ -16,22 +19,17 @@ def calculate_pcc_tax(
     if transaction_type == 'loan':
         
         if amount <= 1000:
-            # Loan amount does not exceed 1000 PLN and lender is outside first tax group
-            # Loan is exempt
+            
             return 0.0
 
-        # Standard tax rate for loans is 0.5%
-        tax = amount * 0.005
+        tax = amount * LOAN_TAX_RATE
         return tax
 
     elif transaction_type == 'car_purchase':
-        # Check for car purchase exemptions
         if amount <= 1000:
-            # Transaction is exempt
             return 0.0
 
-        # Standard tax rate for car purchases is 2%
-        tax = amount * 0.02
+        tax = amount * CAR_PURCHASE_TAX_RATE
         return tax
 
     else:
@@ -39,17 +37,14 @@ def calculate_pcc_tax(
 
 
 def test_anna_loan_scenario():
-    # Parameters based on the scenario
+
     transaction_type = 'loan'
     amount = 20000
 
-    # Calculate the tax
     tax = calculate_pcc_tax(
         transaction_type=transaction_type,
         amount=amount
     )
-
-    # Print the result
     print(f"The PCC tax that Anna needs to pay is: {tax} PLN")
 
 if __name__ == "__main__":
